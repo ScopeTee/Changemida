dca44994e17ae01601bfb1cdadf4053dbbbb1c57
Tayo-Subair Fuad <tayosubairfuad@gmail.com>
2022-09-05 18:41:02 +0100

    section 5 almost done

--- src/components/section5.js
+++ src/components/section5.js
@@ -19,23 +19,23 @@ function Section5() {
             {/* <div className="divider "></div> */}
             <div className="contain s-5 text-center">
                 <section className="mb-5">
-                <h2 className="h2-2">Made for your business</h2>
+                    <h2 className="h2-2 mb-5">Made for your business</h2>
                     <div className="main-flex">
                         <div className="fx-1">
-                        <Image
-                        alt=""
-                        src={Photo17}
-                        width="97"
-                        height="97"
-                        className="d-inline-block align-top "
-                    />{''}
-                    <Image
-                        alt=""
-                        src={Photo18}
-                        width="200"
-                        height="200"
-                        className="d-inline-block align-top "
-                    />{''}
+                            <Image
+                            alt=""
+                            src={Photo17}
+                            width="97"
+                            height="97"
+                            className="d-inline-block align-top "
+                            />{''}
+                            <Image
+                            alt=""
+                            src={Photo18}
+                            width="200"
+                            height="200"
+                            className="d-inline-block align-top mt-5"
+                            />{''}
                         </div>
                         <Image
                         alt=""
@@ -45,51 +45,51 @@ function Section5() {
                         className="d-inline-block align-top "
                     />{''}
                         <div className="fx-2">
-                        <Image
-                        alt=""
-                        src={Photo15}
-                        width="181"
-                        height="181"
-                        className="d-inline-block align-top "
-                    />{''}
-                    <Image
-                        alt=""
-                        src={Photo16}
-                        width="114"
-                        height="114"
-                        className="d-inline-block align-top s4-img pt-5"
-                    />{''}
+                            <Image
+                            alt=""
+                            src={Photo15}
+                            width="181"
+                            height="181"
+                            className="d-inline-block align-top "
+                        />{''}
+                            <Image
+                                alt=""
+                                src={Photo16}
+                                width="114"
+                                height="114"
+                                className="d-inline-block align-top mt-5"
+                            />{''}
                         </div>
 
                     </div>
                 </section>
-                <section className="mt-5">
-                <h2 className="h2-2">Trusted by many</h2>
-                    <div className="main-flex">
-                    <Image
-                        alt=""
-                        src={Image1}
-                        width="126"
-                        height="128"
-                        className="d-inline-block align-top "
-                    />{''}
-                    <Image
-                        alt=""
-                        src={Image2}
-                        width="264"
-                        height="60"
-                        className="d-inline-block align-top"
-                    />{''}
-                    <Image
-                        alt=""
-                        src={Image3}
-                        width="147"
-                        height="68"
-                        className="d-inline-block align-top"
-                    />{''}
+                <section className="my-5">
+                    <h2 className="h2-2">Trusted by many</h2>
+                    <div className="main-flex mb-5">
+                        <Image
+                            alt=""
+                            src={Image1}
+                            width="126"
+                            height="128"
+                            className="d-inline-block align-top "
+                        />{''}
+                        <Image
+                            alt=""
+                            src={Image2}
+                            width="264"
+                            height="60"
+                            className="d-inline-block align-top"
+                        />{''}
+                        <Image
+                            alt=""
+                            src={Image3}
+                            width="147"
+                            height="68"
+                            className="d-inline-block align-top"
+                        />{''}
                     </div>
                 </section>
-                <Button className="btn-2 px-3 py-3">Create yout first wallet</Button>{' '}
+                <Button className="btn-2 px-3 py-3 mt-3">Create yout first wallet</Button>{' '}
             </div>  
         </section>
     );
