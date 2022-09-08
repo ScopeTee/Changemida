0931ea433e2269eac0e78a49c8c66b0aada00534
Tayo-Subair Fuad <tayosubairfuad@gmail.com>
2022-09-08 21:40:51 +0100

    installed dependencies and packages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+import {GlobalStyles} from "./styles/global.styles"
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1 className="container text-white">Hi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/global.styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Nunito', sans-serif;
+        margin: 0 auto;
+        border: 1px solid black;
+    }
+
+`
